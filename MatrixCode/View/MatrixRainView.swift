4bdbd257a93492875ea3d372f91ca90be3bed0e8
ekucet <ekucet@gmail.com>
2022-02-08 14:28:37 +0300

    refactoring

--- MatrixCode/View/MatrixRainView.swift
+++ MatrixCode/View/MatrixRainView.swift
@@ -14,63 +14,56 @@ struct MatrixRainView: View {
             Color.black
             GeometryReader { proxy in
                 let size = proxy.size
-                HStack(spacing: 15) {
-                    ForEach(1...Int(size.width / 25), id: \.self) { _ in
+                HStack {
+                    ForEach(1...Int(size.width / 15), id: \.self) { _ in
                         MatrixRainCharacters(size: size)
                     }
                 }
-                .padding(.horizontal)
             }
         }
     }
 }
 
 struct MatrixRainCharacters: View {
+    
     var size: CGSize
+    private let chars = "0123456789abcdefghijklmnoprstuvwxyzabcquepaje123jdj09"
+    
     @State var startAnimation: Bool = false
     @State var random: Int = 0
+    
     var body: some View {
         let randomHeight: CGFloat = .random(in: (size.height / 2)...size.height)
+        
         VStack {
-            ForEach(0..<constant.count, id: \.self) { index in
-                let char = Array(constant)[getRandomIndex(index: index)]
+            ForEach(0..<chars.count, id: \.self) { index in
+                let char = Array(chars)[getRandomIndex(index: index)]
                 Text(String(char))
-                    .font(.custom("MatrixCodeNFI", size: 25))
+                    .font(.custom("MatrixCodeNFI", size: 15))
                     .foregroundColor(Color("Green"))
             }
         }
         .mask(alignment: .top){
             Rectangle()
-                .fill(
-                    LinearGradient(
-                        colors: [
-                            .clear,
-                            .black.opacity(0.1),
-                            .black.opacity(0.2),
-                            .black.opacity(0.3),
-                            .black.opacity(0.4),
-                            .black.opacity(0.5),
-                            .black.opacity(0.6),
-                            .black.opacity(0.7),
-                            .black],
-                        startPoint: .top,
-                        endPoint: .bottom)
-                )
+                .fill(LinearGradient(
+                    colors: getGradientColors(),
+                    startPoint: .top,
+                    endPoint: .bottom))
                 .frame(height: size.height / 2)
                 .offset(y: startAnimation ? size.height : -randomHeight)
         }
         .onAppear {
-            withAnimation(.linear(duration: 6).delay(.random(in: 0...2)).repeatForever(autoreverses: false)) {
+            withAnimation(.linear(duration: 3).delay(.random(in: 0...2)).repeatForever(autoreverses: false)) {
                 startAnimation = true
             }
         }
         .onReceive(Timer.publish(every: 0.2, on: .main, in: .common).autoconnect()) { _ in
-            random = Int.random(in: 0..<constant.count)
+            random = Int.random(in: 0..<chars.count)
         }
     }
     
-    func getRandomIndex(index: Int) -> Int {
-        let max = constant.count - 1
+    private func getRandomIndex(index: Int) -> Int {
+        let max = chars.count - 1
         if (index + random) > max {
             if (index - random) < 0 {
                 return index
@@ -80,6 +73,18 @@ struct MatrixRainCharacters: View {
             return (index + random)
         }
     }
+    
+    private func getGradientColors() -> [Color] {
+        return [.clear,
+                .black.opacity(0.1),
+                .black.opacity(0.2),
+                .black.opacity(0.3),
+                .black.opacity(0.4),
+                .black.opacity(0.5),
+                .black.opacity(0.6),
+                .black.opacity(0.7),
+                .black]
+    }
 }
 
 struct MatrixRainView_Previews: PreviewProvider {
@@ -88,8 +93,3 @@ struct MatrixRainView_Previews: PreviewProvider {
             .preferredColorScheme(.dark)
     }
 }
-
-
-// MARK: Random Characters
-
-let constant = "0123456789abcdefghijklmnoprstuvwxyzabcquepaje123jdj09"
