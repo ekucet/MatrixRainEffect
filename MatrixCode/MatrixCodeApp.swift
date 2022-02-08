85fb914d7a1a268f71fdcfed31367078b5ee3249
ekucet <ekucet@gmail.com>
2022-02-08 10:33:39 +0300

    Initial Commit

---
+++ MatrixCode/MatrixCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatrixCodeApp.swift
+//  MatrixCode
+//
+//  Created by Erkam Kucet on 8.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct MatrixCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
