7547fa079a763b70240ae70a989cf6446b577e7a
ekucet <ekucet@gmail.com>
2022-02-08 11:15:47 +0300

    First Commit

--- MatrixCode/MatrixCodeApp.swift
+++ MatrixCode/MatrixCodeApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MatrixCodeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MatrixRainView()
         }
     }
 }
---
+++ MatrixCode/View/MatrixRainView.swift
@@ -0,0 +1,95 @@
+//
+//  MatrixRainView.swift
+//  MatrixCode
+//
+//  Created by Erkam Kucet on 8.02.2022.
+//
+
+import SwiftUI
+
+struct MatrixRainView: View {
+    
+    var body: some View {
+        ZStack {
+            Color.black
+            GeometryReader { proxy in
+                let size = proxy.size
+                HStack(spacing: 15) {
+                    ForEach(1...Int(size.width / 25), id: \.self) { _ in
+                        MatrixRainCharacters(size: size)
+                    }
+                }
+                .padding(.horizontal)
+            }
+        }
+    }
+}
+
+struct MatrixRainCharacters: View {
+    var size: CGSize
+    @State var startAnimation: Bool = false
+    @State var random: Int = 0
+    var body: some View {
+        let randomHeight: CGFloat = .random(in: (size.height / 2)...size.height)
+        VStack {
+            ForEach(0..<constant.count, id: \.self) { index in
+                let char = Array(constant)[getRandomIndex(index: index)]
+                Text(String(char))
+                    .font(.custom("MatrixCodeNFI", size: 25))
+                    .foregroundColor(Color("Green"))
+            }
+        }
+        .mask(alignment: .top){
+            Rectangle()
+                .fill(
+                    LinearGradient(
+                        colors: [
+                            .clear,
+                            .black.opacity(0.1),
+                            .black.opacity(0.2),
+                            .black.opacity(0.3),
+                            .black.opacity(0.4),
+                            .black.opacity(0.5),
+                            .black.opacity(0.6),
+                            .black.opacity(0.7),
+                            .black],
+                        startPoint: .top,
+                        endPoint: .bottom)
+                )
+                .frame(height: size.height / 2)
+                .offset(y: startAnimation ? size.height : -randomHeight)
+        }
+        .onAppear {
+            withAnimation(.linear(duration: 6).delay(.random(in: 0...2)).repeatForever(autoreverses: false)) {
+                startAnimation = true
+            }
+        }
+        .onReceive(Timer.publish(every: 0.2, on: .main, in: .common).autoconnect()) { _ in
+            random = Int.random(in: 0..<constant.count)
+        }
+    }
+    
+    func getRandomIndex(index: Int) -> Int {
+        let max = constant.count - 1
+        if (index + random) > max {
+            if (index - random) < 0 {
+                return index
+            }
+            return (index - random)
+        }else {
+            return (index + random)
+        }
+    }
+}
+
+struct MatrixRainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MatrixRainView()
+            .preferredColorScheme(.dark)
+    }
+}
+
+
+// MARK: Random Characters
+
+let constant = "0123456789abcdefghijklmnoprstuvwxyzabcquepaje123jdj09"
